c5dddef5affb3789ff73a7170a788394898282a9
Anilkutsa <anilkutsa@gmail.com>
2020-12-03 09:30:56 +0530

    Fixed mistakes in milvik python file

---
+++ milvik_art.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cd /Users/anilsuryaprakash/Documents/workspace/linux/edex-ui
+npm start
+
+clear
+python /Users/anilsuryaprakash/Documents/workspace/linux/milvik_art/milvik.py
+
+clear
+cd ../nyancat/src
+./nyancat
